5c4c0d53fdc4f7f1744ee5f3018d3f776513b60b
GabrielNarvadez <gab.narvadez@gmail.com>
2023-12-01 23:17:53 +0800

    Updated the templates

--- Parse_app/menus/models.py
+++ Parse_app/menus/models.py
@@ -2,9 +2,45 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    username = models.CharField(max_length = 25)
-    email = models.CharField(max_length = 100)
-    password = models.CharField(max_length = 100)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=True, blank=True)
+    contact_number = models.CharField(max_length=15, null=True, blank=True)
+
     
     def __str__(self):
         return f"{self.username}"
+    
+class Quiz(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duration_minutes = models.PositiveIntegerField()
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.TextField()
+    question_type = models.CharField(max_length=20, choices=[('submit_answer', 'Submit Answer'), ('multiple_choice', 'Multiple Choice')])
+    # Add any other common fields for questions here.
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+class QuizAttempt(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField()
+    score = models.PositiveIntegerField(null=True, blank=True)
+
+class UserResponse(models.Model):
+    attempt = models.ForeignKey(QuizAttempt, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE, null=True, blank=True)
+    submitted_answer = models.TextField(null=True, blank=True)
